f33518057cf77b5132aafd02bad205497b42c08a
marvyty <kopilec691@gmail.com>
2026-09-18 18:56:33 +0300

    feat: add environment configuration for API usage

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,13 +7,14 @@ import {
   withEventReplay,
 } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
+    provideHttpClient(withFetch()),
     provideClientHydration(withEventReplay()),
   ],
 };
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  apiUrl: 'https://api.giphy.com/v1/gifs',
+  apiKey: 'uBKH1VBUZzTmqKPSaTCVbIs3G8LhyVIH',
+};
