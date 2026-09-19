e81d08fe10619d780486677f38574eb297bf21dc
marvyty <kopilec691@gmail.com>
2026-09-19 14:22:12 +0300

    feat: restructure app layout with header component and add styling

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './layout/header/header';
 
 @Component({
   selector: 'gfd-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header],
   templateUrl: './app.html',
+  styleUrls: ['./app.scss'],
 })
 export class App {}
